46829b0a1cdaac838eab0fdc92e1fc49202ce546
Sam <116330181@umail.ucc.ie>
2019-11-07 11:45:51 +0000

    Initial Commit

---
+++ StudentID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentID
+//
+//  Created by Samuel Kiernan on 07/11/2019.
+//  Copyright © 2019 Samuel Kiernan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
